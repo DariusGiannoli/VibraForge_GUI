c8b2f31c1c5fe1d89cbbaad9c1c0e122a976830c
Gabriel TAIEB <gtaieb@MacBook-Pro-de-Gabriel.local>
2025-09-10 23:41:45 +0200

    main at the root

---
+++ Main_GUI/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+import os
+
+
+pattern_generator_dir = os.path.join(os.path.dirname(__file__), "pattern_generator")
+sys.path.insert(0, pattern_generator_dir)
+
+
+from pattern_generator.gui.haptic_pattern_gui import main
+
+if __name__ == "__main__":
+    main()
